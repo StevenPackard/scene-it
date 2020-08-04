45e245619b9b08c3d3e9ce59b022dac79cf2ee0d
Steven Packard <littlepackard@gmail.com>
2020-08-03 21:59:31 -0600

    init server

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace scene_it.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class PostsController : ControllerBase
+  {
+
+
+    [HttpGet]
+
+
+  }
+}
